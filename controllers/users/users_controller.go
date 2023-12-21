55d3e5b8390b17ea9ffb8f3a948e7a7e266e7e0b
Sayeed Mohammad <toothpaste744@gmail.com>
2023-12-21 06:24:30 +0600

    updated & add gin-gonic/gin

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -4,14 +4,31 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"github.com/Sayeed1-dotcom/users-api/domain/users"
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
+	"github.com/Sayeed1-dotcom/users-api/services"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
 	fmt.Println(user)
 	bytes, err :=ioutil.ReadAll(c.Request.Body)
-	fmt.Println(err)
-	fmt.Println(string(bytes))
+	if err !=nil{
+		//TODO: Handle error
+		return
+	}
+    if err:=json.Unmarshal(bytes, &user); err !=nil {
+		fmt.Println(err.Error())
+		//TODO: Handle json error
+		return
+	}
+	
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		//TODO: Handle json error
+		return
+	}
 	c.String(http.StatusNotImplemented, format:"implement me!")
 }
 
---
+++ services/users_service.go
@@ -0,0 +1,10 @@
+package services
+
+import (
+	"github.com/Sayeed1-dotcom/users-api/domain/users"
+) 
+
+
+func CreateUser(user users.User) (*users.User, error) {
+	return nil, nil
+}
